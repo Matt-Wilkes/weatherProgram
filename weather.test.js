bae2d4a55150be5f464b0771dba53e92d882b0b1
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-07-21 21:28:35 +0100

    add mock weatherClient to weather.test.js

--- weather.test.js
+++ weather.test.js
@@ -1,17 +1,48 @@
 // const jestFetchMock = require("jest-fetch-mock").enableMocks();
 
 const Weather = require("./weather");
-const WeatherClient = require('./weatherClient');
-
-// Ill need to mock the weatherClient
+const WeatherClient = require("./weatherClient");
 
 describe("Weather", () => {
-  it("should fetch the weather data for Bristol", (done) => {
-    const weatherClient = new WeatherClient;
+//   Without mock - Using the weatherClient
+  it.skip("should fetch the weather data for Bristol (Skipped)", (done) => {
+    const weatherClient = new WeatherClient();
     const weather = new Weather(weatherClient);
-    weather.load('Bristol').then((data) => {
-        expect(data.name).toEqual('Bristol');
-        done();
-    })
+    weather.load("Bristol").then((data) => {
+      expect(data.name).toEqual("Bristol");
+      done();
+    });
+  });
+//   With mocks, Mocking the weatherClient
+  it("load should fetch the weather data for Bristol", (done) => {
+    const mockClient = {
+      fetchWeatherData: jest.fn(), // This is a jest mock function, mocks WeatherClient.fetchWeatherData()
+    };
+    // mocks returning the 'name' property from fetchWeatherData
+    mockClient.fetchWeatherData.mockResolvedValueOnce({
+      name: "Bristol",
+    });
+    // New instance of Weather, Passing mock in as the argument
+    const weather = new Weather(mockClient);
+    // Used the same function call as with the non-mocked code
+    weather.load("Bristol").then((data) => {
+      expect(data.name).toEqual("Bristol");
+      done();
+    });
+  });
+  it("should return the weather data for Bristol", async () => {
+    const mockClient = {
+      fetchWeatherData: jest.fn(),
+    };
+    mockClient.fetchWeatherData.mockResolvedValueOnce({
+      name: "Bristol",
+    });
+ 
+    const weather = new Weather(mockClient);
+    await weather.load("Bristol");
+    await expect(weather.getWeatherData()).toEqual({
+        name: "Bristol",
+      });
+    
   });
 });
